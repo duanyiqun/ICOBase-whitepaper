f0ab7fd8e26b0b2596057c64655d32a330ad7f11
Claude Duan <claude.duan@2hatsecurity.com>
2019-02-10 22:15:03 -0800

    Update train_cls_txtcnn.py

--- train_cls_txtcnn.py
+++ train_cls_txtcnn.py
@@ -31,7 +31,8 @@ parser.add_argument('--linear_dim',default=1000, type=str, help='emb_dim size')
 parser.add_argument('--num_classes',default=5, type=str, help='emb_dim size')
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'
 best_acc = 0
 start_epoch = 0
 train_data = dataloader.Myarticles(args.csvdir,args.article_dir,validation=False)
